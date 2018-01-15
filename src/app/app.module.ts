39de6f1ddafcee9c8294e5f9c6b42e691683e83f
Antonio Marin <antoniomarinnn@gmail.com>
2018-01-15 14:28:30 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import {MatButtonModule, MatGridListModule, MatToolbarModule} from '@angular/material';
+import { MatButtonModule, MatGridListModule, MatToolbarModule } from '@angular/material';
+import { RouterModule, Routes } from '@angular/router';
+import { CvComponent } from './cv/cv.component';
+import { ProjectsComponent } from './projects/projects.component';
 
+const appRoutes: Routes = [
+  { path: 'cv',
+    component: CvComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  { path: '',
+    redirectTo: '/cv',
+    pathMatch: 'full'
+  },
+  { path: '**',  redirectTo: '' }
+  // { path: '**', component: PageNotFoundComponent } TODO 404 page
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CvComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
     MatButtonModule,
     MatGridListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/cv/cv.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cv',
+  templateUrl: '../cv/cv.component.html',
+  styleUrls: ['../cv/cv.component.css']
+})
+
+export class CvComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
