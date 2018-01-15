1fbda1a7a3db75122a69e804f083de03583561ed
Antonio Marin <antoniomarinnn@gmail.com>
2018-01-15 23:37:40 +0100

    Almost done, only thing left is to finish projects page. Btw we're on dark side now :)

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: '../projects/projects.component.html',
+  styleUrls: ['../projects/projects.component.css']
+})
+
+export class ProjectsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
